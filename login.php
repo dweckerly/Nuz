9a418d105f9f9576b0f612c7d20d0ccafd2ec3be
dweckerly <d.weckerly@gmail.com>
2018-03-09 11:07:13 -0600

    Error messages for login page

--- login.php
+++ login.php
@@ -10,6 +10,24 @@ include_once('layout/header.php');
             <input type="text" name="password" placeholder="yur paswerd">
             <button type="submit" name="submit">Log In</button>
         </form>
+        <?php
+        if(!empty($_GET['login'])) {
+            $logErr = $_GET['login'];
+            if($logErr == 'notfound') {
+                echo "<div class='alert alert-danger' role='alert'>No account found.</div>";
+            } elseif($logErr == 'inactive') {
+                echo "<div class='alert alert-danger' role='alert'>This account has been deactivated.</div>";
+            } elseif($logErr == 'pwd') {
+                echo "<div class='alert alert-danger' role='alert'>Password doesn't match.</div>";
+            } elseif($logErr == 'empty') {
+                echo "<div class='alert alert-danger' role='alert'>Fill out both of these ^^</div>";
+            } elseif($logErr == 'invalid') {
+                echo "<div class='alert alert-danger' role='alert'>That email looks a little funny...</div>";
+            } else {
+                echo "<div class='alert alert-danger' role='alert'>" . $logErr ."</div>";
+            }
+        }
+        ?>
     </div>
 </section>
 
