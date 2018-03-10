a453368bb920735b6cb6a351a65685e50324cba9
David <d.weckerly@gmail.com>
2018-03-10 09:43:57 -0600

    testing js

--- js/startGame.js
+++ js/startGame.js
@@ -1,12 +1,15 @@
 var txt = [
     "Howdy pardner! You must be the one that inherited The Ranch from your long-lost, eccentric, twice-removed, felon of an Uncle.",
     "Let me be the first to say... Welcome! Name's Pete. I been a ranch-hand here for awhile now, so I'll show you around the place.",
-    "What's your name?"];
+    "What's your name?"
+];
 
 var click = 0;
 
 function start() {
-    if(click < txt.length()) {
+    console.log("function started");
+    if (click < txt.length()) {
+        console.log("typewriter started");
         typeWriter(txt[click], 'startText');
     } else {
         nameModal();
@@ -15,6 +18,7 @@ function start() {
 
 function nextText() {
     click++;
+    console.log(click);
     start();
 }
 
