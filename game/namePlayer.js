f44a60c63d9cb8b08bc761e47568a5d3b40ca52e
dweckerly <d.weckerly@gmail.com>
2018-03-16 10:14:06 -0500

    file move

--- game/namePlayer.js
+++
@@ -1,5 +0,0 @@
-var txt = [
-    "Howdy pardner! This is The Ranch. It was left to you by your long-lost, eccentric, twice-removed, felon of an Uncle.",
-    "Let me be the first to say... Welcome! Name's Pete Moss. I been a ranch-hand here for awhile now, so I'll show you around the place.",
-    "What's your name?"
-];
