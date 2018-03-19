9e95208c7f72b040c1a618d6f4efe0dfd1179fd3
dweckerly <d.weckerly@gmail.com>
2018-03-19 13:23:33 -0500

    Delete db.inc.php

--- includes/db.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbServerName = "localhost";
-$dbUserName = "u795937052_dweck";
-$dbPassword = "R@n$0m!?!";
-$dbName = "u795937052_nuz";
-
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
