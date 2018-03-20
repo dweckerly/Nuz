40d4f07c4416cf01dcdebbf8fab670d07a5fd6fd
dweckerly <d.weckerly@gmail.com>
2018-03-20 15:28:19 -0500

    Added rmon page

--- rmon.php
+++ rmon.php
@@ -1,10 +1,24 @@
 <?php
 include_once('layout/header.php');
-
-// TODO:
-// would like to make this page display a random mon
+include_once('includes/db.inc.php');
+$num = rand(1, 8);
+$sql = "SELECT name, description, imgPath FROM mons WHERE monID = '$num'";
+$resut = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($resut);
 ?>
+<div class="container" align="center">
+    <div class="card text-center" style="width: 18rem;">
+        <img class="card-img-top" src="<?php echo $row['imgPath']; ?>">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $row['name']; ?></h5>
+            <p class="card-text"><?php echo $row['description'] ?></p>
+        </div>
+    </div>
+    <br />
+    <input type="button" class="btn btn-outline-secondary" value="Again!" onClick="window.location.reload()">
+</div>
 
 <?php
+mysqli_close($conn);
 include_once('layout/footer.php');
 ?>
--- sections/playerInfo.php
+++ sections/playerInfo.php
@@ -1,13 +1,28 @@
 <?php
+if(!empty($_SESSION['gid'])) {
+    include_once("../includes/db.inc.php");
+    $gid = $_SESSION['gid'];
+    $sql = "SELECT * FROM games WHERE gameID = '$gid'";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if($resultCheck > 0) {
+        // show player information
+    } else {
+        // show create game dialogue
+    }
+} else {
+    // empty gid in session... ¯\_(ツ)_/¯
+}
+
 /*
- * Name, location, time, number of mons,
+ * Name, location, time, number of mons, (?)
  * button to start game
  *
  * if no info give option to start new game
  */
 ?>
 <p>This is where retrieved information about the player will be</p>
-<?php 
-if()
-<button class="btn btn-outline-secondary" type="submit">Play Game!</button>
 
+<form action="../util/loadGame.php" method="POST">
+    <button class="btn btn-outline-secondary" type="submit" name="submit">Play Game!</button>
+</form>
