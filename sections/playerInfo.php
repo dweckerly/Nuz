a04dffd6ea9245d288502da21103f8d0713e73ff
dweckerly <d.weckerly@gmail.com>
2018-03-20 15:52:00 -0500

    get player info to display logic

--- sections/playerInfo.php
+++ sections/playerInfo.php
@@ -6,12 +6,36 @@ if(!empty($_SESSION['gid'])) {
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
     if($resultCheck > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $locID = $row['locationID'];
+        $day = $row['day'];
+        $time = $row['time'];
+        $sql = "SELECT name FROM locations WHERE locationID = '$locID'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $loc = $row['name'];
+        if(empty($_SESSION['pid'])){
+            $pid = $row['playerID'];
+            $_SESSION['pid'] = $pid;
+        } else {
+            $pid = $_SESSION['pid'];
+        }
+        // get player name from player table
         // show player information
-    } else {
+    } else {?>
+
+    <?php
         // show create game dialogue
-    }
+    } ?>
+<form action="../util/loadGame.php" method="POST">
+    <button class="btn btn-outline-secondary" type="submit" name="submit">Play Game!</button>
+</form>
+<?php
 } else {
     // empty gid in session... ¯\_(ツ)_/¯
+    ?>
+    <p>You ain't got no info...</p>
+    <?php
 }
 
 /*
@@ -21,8 +45,3 @@ if(!empty($_SESSION['gid'])) {
  * if no info give option to start new game
  */
 ?>
-<p>This is where retrieved information about the player will be</p>
-
-<form action="../util/loadGame.php" method="POST">
-    <button class="btn btn-outline-secondary" type="submit" name="submit">Play Game!</button>
-</form>
