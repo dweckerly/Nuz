71c67eaf66d5ed3b2925ac275e4a3b25c34e0deb
David <d.weckerly@gmail.com>
2018-04-06 12:15:32 -0500

    added favicon

--- layout/header.php
+++ layout/header.php
@@ -9,6 +9,7 @@ if(!isset($_SESSION['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="../img/favicon.png"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
     <link href="../css/style.css" rel="stylesheet">
