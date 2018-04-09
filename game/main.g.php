df69f21a397473fc6e0dd59b7147a9d2ccc47569
dweckerly <d.weckerly@gmail.com>
2018-04-09 10:07:16 -0500

    main game work

--- game/main.g.php
+++ game/main.g.php
@@ -6,20 +6,20 @@ include_once("../layout/gameHeader.php");
         <!-- currentlt just doing a 3 x 3 grid for the map-->
         <div id="0" class="row">
             <div id="0-0" class="col">
-                <button><img src="" /></button>
+                <button><i class="fa fa-anchor" style="font-size:36px"></i></button>
             </div>
-            <div id="0-1" class="col"></div>
-            <div id="0-2" class="col"></div>
+            <div id="0-1" class="col"><button><i class="fa fa-birthday-cake" style="font-size:36px"></i></button></div>
+            <div id="0-2" class="col"><button><i class="fa fa-bug" style="font-size:36px"></i></button></div>
         </div>
         <div id="1" class="row">
-            <div id="1-0" class="col"></div>
-            <div id="1-1" class="col"></div>
-            <div id="1-2" class="col"></div>
+            <div id="1-0" class="col"><button><i class="fa fa-diamond" style="font-size:36px"></i></button></div>
+            <div id="1-1" class="col"><button><i class="fa fa-fire" style="font-size:36px"></i></button></div>
+            <div id="1-2" class="col"><button><i class="fa fa-leaf" style="font-size:36px"></i></button></div>
         </div>
         <div id="2" class="row">
-            <div id="2-0" class="col"></div>
-            <div id="2-1" class="col"></div>
-            <div id="2-2" class="col"></div>
+            <div id="2-0" class="col"><button><i class="fa fa-rocket" style="font-size:36px"></i></button></div>
+            <div id="2-1" class="col"><button><i class="fa fa-snowflake-o" style="font-size:36px"></i></button></div>
+            <div id="2-2" class="col"><button><i class="fa fa-ship" style="font-size:36px"></i></button></div>
         </div>
     </div>
 </div>
--- layout/gameHeader.php
+++ layout/gameHeader.php
@@ -1,13 +1,27 @@
-<div class="row">
-    <div class="col-md-2 d-none d-md-block sidebar">
-        <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-                <li class="nav-item"><button class="btn"><img src="../img/ui/dex.jpg" class="img-fluid"></button></li>
-                <li class="nav-item"><button class="btn"><img src="../img/ui/mons.jpg" class="img-fluid"></button></li>
-                <li class="nav-item"><button class="btn"><img src="../img/ui/inventory.jpg" class="img-fluid"></button></li>
-                <li class="nav-item"><button class="btn"><img src="../img/ui/player.jpg" class="img-fluid"></button></li>
-                <li class="nav-item"><button class="btn"><img src="../img/ui/journal.jpg" class="img-fluid"></button></li>
-            </ul>
-        </div>
+<!DOCTYPE html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="../img/favicon.png"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <link href="../css/style.css" rel="stylesheet">
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/ajax.js"></script>
+    <script src="../js/uiEffects.js"></script>
+    <title>NuzMon</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="col-md-2 d-none d-md-block sidebar">
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item"><button class="btn"><img src="../img/ui/dex.jpg" class="img-fluid"></button></li>
+            <li class="nav-item"><button class="btn"><img src="../img/ui/mons.jpg" class="img-fluid"></button></li>
+            <li class="nav-item"><button class="btn"><img src="../img/ui/inventory.jpg" class="img-fluid"></button></li>
+            <li class="nav-item"><button class="btn"><img src="../img/ui/player.jpg" class="img-fluid"></button></li>
+            <li class="nav-item"><button class="btn"><img src="../img/ui/journal.jpg" class="img-fluid"></button></li>
+        </ul>
     </div>
 </div>
