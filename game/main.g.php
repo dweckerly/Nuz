0e83bb3ccc2a5cbb2d7372588da965ff62c2cb38
dweckerly <d.weckerly@gmail.com>
2018-04-09 09:56:28 -0500

    Working on main map

--- game/main.g.php
+++ game/main.g.php
@@ -1,9 +1,29 @@
 <?php
-include_once("../layout/gHeader.php");
+include_once("../layout/gameHeader.php");
 ?>
 <div id="mainContainer">
-coming sooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooon
+    <div id="map">
+        <!-- currentlt just doing a 3 x 3 grid for the map-->
+        <div id="0" class="row">
+            <div id="0-0" class="col">
+                <button><img src="" /></button>
+            </div>
+            <div id="0-1" class="col"></div>
+            <div id="0-2" class="col"></div>
+        </div>
+        <div id="1" class="row">
+            <div id="1-0" class="col"></div>
+            <div id="1-1" class="col"></div>
+            <div id="1-2" class="col"></div>
+        </div>
+        <div id="2" class="row">
+            <div id="2-0" class="col"></div>
+            <div id="2-1" class="col"></div>
+            <div id="2-2" class="col"></div>
+        </div>
+    </div>
 </div>
+<script src="../js/mapMaker.js"></script>
 <?php
-include_once("../layout/gFooter.php");
+include_once("../layout/gameFooter.php");
 ?>
